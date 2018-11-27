d0ba0c6ca24d1dec488b183c9328eb37784b431d
Lukasz Obara <lukasz.obara@pslgroup.com>
2018-11-27 14:22:17 -0500

    added generate cmd function

--- src/main/java/com/lukasz/make_json/cliParser.java
+++ src/main/java/com/lukasz/make_json/cliParser.java
@@ -1,7 +1,13 @@
 package com.lukasz.make_json;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+
 
 public class cliParser {
     public static Options generateArguments(){
@@ -17,5 +23,17 @@ public class cliParser {
         return options;
     }
 
-    public static void generateCommandLine(){} 
+
+    public static void generateCommandLine(final Options options, final String[] args){
+        final CommandLineParser parser = new DefaultParser();
+        final HelpFormatter formatter = new HelpFormatter();
+        CommandLine cmd = null;
+
+        try {
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            System.err.println(e.getMessage());
+            formatter.printHelp("utility-name", options);
+        }
+    } 
 }
