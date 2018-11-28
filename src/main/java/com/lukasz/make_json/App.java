9a24b75f7e0fc77788fc734d44c144e1ba3c3e7c
Lukasz Obara <lukasz48@gmail.com>
2018-11-27 23:14:48 -0500

    workign on parseJson method

--- src/main/java/com/lukasz/make_json/App.java
+++ src/main/java/com/lukasz/make_json/App.java
@@ -1,16 +1,48 @@
 package com.lukasz.make_json;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
+import org.json.JSONObject;
 
 
 public class App {
-    public static void main(String[] args) {
-        String loc;
+    public static List<File> getJsons(String loc) throws IOException {
+        List<File> filesInFolder = Files.walk(Paths.get(loc))
+                                        .filter(Files::isRegularFile)
+                                        .map(Path::toFile)
+                                        .collect(Collectors.toList());
+        
+        return filesInFolder;
+    }
 
+    
+    public static void parseJson(File loc){
+        JSONObject jsonObject = new JSONObject(loc);
+        System.out.println(jsonObject.get("index"));
+    }
+
+    public static void main(String[] args) throws IOException {
         Options options = cliParser.generateArguments();
         CommandLine cmd = cliParser.generateCommandLine(options, args);
 
-        System.out.println("The value of " + cmd.getOptionValue("input", "default_value"));
+        String loc = "/home/lukasz/Documents/data";
+        String locFiles = cmd.getOptionValue("input", loc);
+        
+        List<File> filesInFolder = getJsons(locFiles);
+        
+        for (File file : filesInFolder){
+            parseJson(file);
+        }
+        // filesInFolder.stream()
+        //     .forEach(System.out::println);
+        // System.out.println(filesInFolder);
     }
 }
