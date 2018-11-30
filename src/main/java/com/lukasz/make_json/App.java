3c9bc168fd42fd456cd71a67e975114c52b67988
Lukasz Obara <lukasz48@gmail.com>
2018-11-29 23:30:00 -0500

    .

--- src/main/java/com/lukasz/make_json/App.java
+++ src/main/java/com/lukasz/make_json/App.java
@@ -3,22 +3,23 @@ package com.lukasz.make_json;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
+// import java.io.FileNotFoundException;
+// import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.nio.file.FileSystems;
+import java.io.PrintWriter;
+// import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+// import java.util.stream.Stream;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Options;
-import org.apache.commons.io.FileUtils;
+// import org.apache.commons.io.FileUtils;
 // import org.apache.commons.cli.Parser;
 import org.json.JSONObject;
 
@@ -35,31 +36,33 @@ public class App {
     }
 
 
-    public static void parseJson(String loc) throws IOException {
+    public static String parseJson(String loc) throws IOException {
         InputStream file = new FileInputStream(new File(loc));
         BufferedReader bufferReader = new BufferedReader(new InputStreamReader(file));
         String data = bufferReader.lines()
             .collect(Collectors.joining("\n"));
-        System.out.println(data);
-        // JSONObject jsonObject = new JSONObject(new FileReader(data));
-        // System.out.println(jsonObject);
-        // System.out.println(jsonObject.get("index"));
+
+        JSONObject jsonObject = new JSONObject(data);
+
+        bufferReader.close();
+        return jsonObject.toString(4);
     }
 
     public static void main(String[] args) throws IOException {
         Options options = cliParser.generateArguments();
         CommandLine cmd = cliParser.generateCommandLine(options, args);
 
-        String loc = "/home/lukasz.obara/Lukasz";
+        String loc = "/home/lukasz/Documents/data";
         String locFiles = cmd.getOptionValue("input", loc);
         
-        // getJsons(locFiles);
         List<String> filesInFolder = getJsons(locFiles);
         
         for (String file : filesInFolder){
-            // System.out.println(file);
-            parseJson(file);
-            System.out.println("--------");
+            String json = parseJson(file);
+
+            try (PrintWriter out = new PrintWriter(file)) {
+                out.println(json);
+            }
         }
     }
 }
